7a63ad8f10dbe2a4567b5991acba5b05fe276481
Skylar Kline <skylarkline16@gmail.com>
2025-01-30 15:02:21 -0800

    Added basic pages

---
+++ src/app/(auth)/ForgotPassword/page.jsx
@@ -0,0 +1,9 @@
+const ForgotPasswordPage = () => {
+  return (
+    <div>
+      <h1>Forgot Password Page</h1>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
---
+++ src/app/(auth)/Register/page.jsx
@@ -0,0 +1,9 @@
+const RegistrationPage = () => {
+  return (
+    <div>
+      <h1>Registration Page</h1>
+    </div>
+  );
+};
+
+export default RegistrationPage;
