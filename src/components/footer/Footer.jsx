f41b9875d107349812751618a2c1cb3fd85be175
Skylar Kline <skylarkline16@gmail.com>
2025-02-01 20:07:18 -0800

    footer, app page, about pages and css applied

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,12 @@
+import styles from './footer.module.css';
+
 const Footer = () => {
     return (
-      <div>
-        Footer
+      <div className={styles.container}>
+        <div className={styles.logo}>FOOTER</div>
+        <div className={styles.text}>
+          Filler text, @ rights reserved
+        </div>
       </div>
     );
   };
