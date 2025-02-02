4f6500feb531ee4afadf54ce08682c0e00d0a6eb
Skylar Kline <skylarkline16@gmail.com>
2025-02-01 20:19:31 -0800

    about and contact

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
+export const metadata = { title: "About Page", description: "About page of the blog" };
+
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
-  );
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About NAME</h2>
+        <h1 className={styles.title}>Short sales pitch</h1>
+        <p className={styles.desc}>Long sales pitch</p>
+        <div className={styles.box}>
+          <div className={styles.box}><h1>#</h1><p>text</p></div>
+          <div className={styles.box}><h1>#</h1><p>text</p></div>
+          <div className={styles.box}><h1>#</h1><p>text</p></div>
+          </div>
+            </div>
+            <div className={styles.imgContainer}>
+                <Image src="/about.png" alt="About Image" fill className={styles.img} />
+            </div>
+        </div>
+    );
 };
 
 export default AboutPage;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,34 @@
-const ContactPage = () => {
-  return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
-  );
+import Image from "next/image";
+import styles from "./contact.module.css";
+
+export const metadata = {
+  title: "Contact",
+  description: "Contact desc",
+};
+
+  const ContactPage = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src="/contact.png" alt="" fill className={styles.img} />
+            </div>
+            <div className={styles.formContainer}>
+                <form action="" className={styles.form}>
+                    <input type="text" placeholder="First and Last Name" />
+                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Phone Number (Optional)" />
+                    <textarea
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="10"
+                        placeholder="Message"
+                    ></textarea>
+                    <button>Send</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default ContactPage;
